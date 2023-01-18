4fb67bdbfbadf5cc59c04fd86ed020297d53e075
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2023-01-19 06:21:01 +1300

    Fix incorrect header

--- src/helpers.h
+++ src/helpers.h
@@ -114,7 +114,6 @@ int64_t readConfigInt (toml_table_t *table, const char *key, int64_t notFoundVal
 char *readConfigString (toml_table_t *table, const char *key, char *notFoundValue);
 Vec2 createVec2 (float x, float y);
 Vec3 createVec3 (float x, float y, float z);
-Vec4 getPlaceholderRect (float *placeholderData);
 bool vec4ContainsVec2 (Vec4 box, Vec2 location);
 #ifdef __cplusplus
 }
