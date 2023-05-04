4f238a4b2e60cddbcca5eca21840222af02377b3
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2023-05-05 06:30:47 +1200

    fix: Ranks now properly work on result

--- src/mod.cpp
+++ src/mod.cpp
@@ -126,8 +126,50 @@ HOOK (bool, StageResultSwitchLoaded, 0x1401E8060) { return *(i32 *)(implOfGetSta
 
 bool
 GameResultLoop (u64 task) {
-	if (*(bool *)0x140DAB380 && *(i32 *)0x140DAB388 == 0) *(i32 *)0x141103E34 = 2;
+	if (*(bool *)0x140DAB380 && *(i32 *)0x140DAB388 == 0 && *(i32 *)(task + 0x68) < 8) {
+		*(i32 *)(task + 0x68) = 8;
+		*(i32 *)(task + 0x88) = 1;
+	}
 	return false;
+}
+
+i32 fsRank;
+i32 ctRank;
+i32 fsPoints;
+i32 ctPoints;
+FUNCTION_PTR (void, GetFSCTRankData, 0x1401E7C60, i32 *fsRank, i32 *ctRank, i32 *fsPoints, i32 *ctPoints);
+
+bool
+PVGameInit (u64 task) {
+	GetFSCTRankData (&fsRank, &ctRank, &fsPoints, &ctPoints);
+	return false;
+}
+
+HOOK (i32 *, GetRankData, 0x1401E7C50) {
+	bool isFutureSound;
+	asm ("mov %0, byte ptr [rbx + 0x1FF]" : "=g"(isFutureSound));
+	i32 rank;
+	i32 points;
+	i32 *pointsRequired = (i32 *)0x1412B6828;
+	pointsRequired += (39 * isFutureSound);
+	if (isFutureSound) {
+		rank   = fsRank;
+		points = fsPoints;
+	} else {
+		rank   = ctRank;
+		points = ctPoints;
+	}
+
+	i32 *data = originalGetRankData ();
+
+	data[0]  = *(i32 *)0x14CC08DD0; // Gained points
+	data[1]  = points;
+	data[2]  = rank;
+	data[4]  = pointsRequired[rank - 1];
+	data[5]  = pointsRequired[rank];
+	data[14] = 1;
+
+	return data;
 }
 
 extern "C" {
@@ -159,6 +201,7 @@ init () {
 	INSTALL_HOOK (GetStageResultSwitch);
 	INSTALL_HOOK (StageResultSwitchFinished);
 	INSTALL_HOOK (StageResultSwitchLoaded);
+	INSTALL_HOOK (GetRankData);
 	INSTALL_HOOK (CmnMenuTouchCheck);
 
 	// 1.00 Samyuu, 1.03 BroGamer
@@ -203,6 +246,10 @@ init () {
 	diva::taskAddition gameResultAddition;
 	gameResultAddition.loop = GameResultLoop;
 	diva::addTaskAddition ("GameResultTask", gameResultAddition);
+
+	diva::taskAddition pvGameAddition;
+	pvGameAddition.init = PVGameInit;
+	diva::addTaskAddition ("PVGAME", pvGameAddition);
 }
 
 void
